a07e9fe72afad20a30f7f9f0f4397b193398b500
Katerina Savidaki <ksavvidaki@gmail.com>
2025-06-19 13:58:33 +0300

    feat: enum for roletypes created

---
+++ backend/src/main/java/com/taskflow/backend/core/enums/RoleType.java
@@ -0,0 +1,14 @@
+package com.taskflow.backend.core.enums;
+
+/**
+ * Enum representing the available user roles in the system.
+ * <p>
+ * Each role defines the level of access and permissions
+ * a user has within the application.
+ */
+public enum RoleType {
+    ADMIN,
+    MANAGER,
+    TEAM_LEADER,
+    MEMBER
+}
