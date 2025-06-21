1f24d7fe1da77ad32a47d1f4c7f727a6a14e482c
Katerina Savidaki <ksavvidaki@gmail.com>
2025-06-21 15:32:35 +0300

    feat: add isCompleted field and update task assignment methods for bidirectional consistency

--- backend/src/main/java/com/taskflow/backend/model/Task.java
+++ backend/src/main/java/com/taskflow/backend/model/Task.java
@@ -4,6 +4,7 @@ import com.taskflow.backend.core.enums.TaskPriorityType;
 import com.taskflow.backend.core.enums.TaskStatusType;
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
 
 import java.time.LocalDateTime;
 
@@ -38,14 +39,47 @@ public class Task extends AbstractEntity {
     @Column(name="due_date")
     private LocalDateTime dueDate;
 
+    @Column(name = "is_completed", nullable = false)
+    @ColumnDefault("false")
+    private Boolean isCompleted;
+
+    // Many-to-One: Many tasks can be created by one user
     @ManyToOne
     @JoinColumn(name = "created_by_id", nullable = false)
     private User createdBy;
 
+    // Many-to-One: Many tasks can be assigned to one user
     @Column(name = "assigned_to_id")
     @ManyToOne
     private User assignedTo;
 
+    /**
+     * Sets the creator of this task and maintains bidirectional consistency.
+     */
+    public void setCreatedBy(User user) {
+        if (this.createdBy != null) {
+            this.createdBy.getCreatedTasks().remove(this);
+        }
 
+        this.createdBy = user;
+        if (user != null && !user.getCreatedTasks().contains(this)) {
+            user.getCreatedTasks().add(this);
+        }
+    }
+
+    /**
+     * Sets the assignee of this task and maintains bidirectional consistency.
+     */
+    public void setAssignedTo(User user) {
+        if (this.assignedTo != null) {
+            this.assignedTo.getAssignedTasks().remove(this);
+        }
+
+        this.assignedTo = user;
+        if (user != null && !user.getAssignedTasks().contains(this)) {
+            user.getAssignedTasks().add(this);
+        }
+
+    }
 }
 
