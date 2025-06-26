48a46365fb4e65a2e9a37f6e69ab673032479fd8
Katerina Savidaki <ksavvidaki@gmail.com>
2025-06-27 00:56:45 +0300

    feat: add methods to find User by AFM and phone number in UserRepository

--- backend/src/main/java/com/taskflow/backend/repository/UserRepository.java
+++ backend/src/main/java/com/taskflow/backend/repository/UserRepository.java
@@ -4,11 +4,14 @@ import com.taskflow.backend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import javax.swing.text.html.Option;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
 
     Optional<User> findByUsername(String username);
     Optional<User> findByUuid(String uuid);
+    Optional<User> findByAfm(String afm);
+    Optional<User> findByPhoneNumber(String phoneNumber);
     Boolean existsByUsername(String username);
 }
