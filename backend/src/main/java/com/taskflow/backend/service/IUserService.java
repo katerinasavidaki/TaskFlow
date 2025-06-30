42b2030e05dce49cc9603d956d39771f774e7d57
Katerina Savidaki <ksavvidaki@gmail.com>
2025-06-30 16:43:28 +0300

    refactor: update IUserService to use UserRegisterDTO for user creation

--- backend/src/main/java/com/taskflow/backend/service/IUserService.java
+++ backend/src/main/java/com/taskflow/backend/service/IUserService.java
@@ -1,13 +1,13 @@
 package com.taskflow.backend.service;
 
-import com.taskflow.backend.dto.UserInsertDTO;
+import com.taskflow.backend.dto.UserRegisterDTO;
 import com.taskflow.backend.dto.UserReadOnlyDTO;
 import com.taskflow.backend.dto.UserUpdateDTO;
 
 import java.util.List;
 
 public interface IUserService {
-    UserReadOnlyDTO createUser(UserInsertDTO insertDTO);
+    UserReadOnlyDTO createUser(UserRegisterDTO insertDTO);
     UserReadOnlyDTO updateUser(Long id, UserUpdateDTO updateDTO);
     void deleteUser(Long id);
     UserReadOnlyDTO getUserById(Long id);
