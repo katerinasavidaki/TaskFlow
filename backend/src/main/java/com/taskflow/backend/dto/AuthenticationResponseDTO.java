68ee5fdf960b2590932f0a18fdc9e309685b339f
Katerina Savidaki <ksavvidaki@gmail.com>
2025-06-30 16:44:34 +0300

    feat: add AuthenticationResponseDTO for user authentication response handling

---
+++ backend/src/main/java/com/taskflow/backend/dto/AuthenticationResponseDTO.java
@@ -0,0 +1,17 @@
+package com.taskflow.backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AuthenticationResponseDTO {
+
+    private String firstname;
+    private String lastname;
+    private String token;
+}
